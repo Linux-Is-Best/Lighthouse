47ec69ccd53fcce572382470453c1a376d8c464c
Linux-Is-Best <opensuse@duck.com>
2024-10-10 10:27:36 -0400

    Update build.sh
    Add podman podman-docker podman-machine distrobox qemu virt-manager htop

--- build.sh
+++ build.sh
@@ -13,10 +13,10 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install screen
+rpm-ostree install podman podman-docker podman-machine distrobox qemu virt-manager htop
 
 # this would install a package from rpmfusion
-# rpm-ostree install vlc
+# rpm-ostree install vlc 
 
 #### Example for enabling a System Unit File
 
